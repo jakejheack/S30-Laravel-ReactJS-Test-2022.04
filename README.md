5f6b3271c26b965fbfcbb12f8d728f42dceadcab
jheackjardinel <jheackjardinel111@gmail.com>
2025-09-07 05:54:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# S30-Laravel-ReactJS-Test-2022.04
---
+++ backend/config/cors.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines which domains are allowed to access your
+    | application's API via the browser. You may enable CORS for a route or
+    | a group of routes by adding the `cors` middleware to the route's middleware
+    | group.
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['http://localhost:3000'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
